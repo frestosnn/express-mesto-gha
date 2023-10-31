d0d55eb25831932deedbffd6ec4b6cc526365f03
frestosnn <fresto.n@yandex.ru>
2023-10-31 17:41:43 +0300

    test

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const helmet = require("helmet");
-const { createUser, login } = require("./controllers/users");
-require("dotenv").config();
-const auth = require("./middlewares/auth");
-const { celebrate, Joi } = require("celebrate");
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+const { createUser, login } = require('./controllers/users');
+require('dotenv').config();
+const auth = require('./middlewares/auth');
 
-const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } =
-  process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 // через localhost не получается подключиться, первая ссылка с решением проблемы со StackOverflow
 mongoose.connect(DB_URL);
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
 
-app.post("/signin", login);
+app.post('/signin', login);
 app.post(
-  "/signup",
+  '/signup',
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
@@ -34,18 +34,18 @@ app.post(
       password: Joi.string().required().min(8),
     }),
   }),
-  createUser
+  createUser,
 );
 
 app.use(auth);
 
-app.use("/users", require("./routes/users"));
-app.use("/cards", require("./routes/cards"));
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.use(errors());
 
 app.use((req, res, next) => {
-  const err = new Error("Not Found: Маршрут не найден");
+  const err = new Error('Not Found: Маршрут не найден');
   err.status = 404;
   next(err);
 });
