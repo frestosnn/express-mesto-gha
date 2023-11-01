cf56f157ace98f539d4f4c784fed757cf31ec3d1
frestosnn <fresto.n@yandex.ru>
2023-11-01 16:40:11 +0300

    еуые

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,27 @@
-const router = require('express').Router();
-const { celebrate, Joi, Segments } = require('celebrate');
+const router = require("express").Router();
+const { celebrate, Joi, Segments } = require("celebrate");
 const {
   getUsers,
   getUser,
   updateUser,
   updateUserAvatar,
   getOwner,
-} = require('../controllers/users');
-const auth = require('../middlewares/auth');
+} = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
-router.get('/', auth, getUsers);
+router.get("/", auth, getUsers);
 router.get(
-  '/:userId',
+  "/:userId",
   auth,
   celebrate({
     [Segments.PARAMS]: {
       userId: Joi.string().hex().length(24).required(),
     },
   }),
-  getUser,
+  getUser
 );
 router.patch(
-  '/me',
+  "/me",
   auth,
   celebrate({
     body: Joi.object().keys({
@@ -29,18 +29,18 @@ router.patch(
       about: Joi.string().min(2).max(30).required(),
     }),
   }),
-  updateUser,
+  updateUser
 );
 router.patch(
-  '/me/avatar',
+  "/me/avatar",
   auth,
   celebrate({
     body: Joi.object().keys({
       avatar: Joi.string().required(),
     }),
   }),
-  updateUserAvatar,
+  updateUserAvatar
 );
-router.get('/me', auth, getOwner);
+router.get("/me", auth, getOwner);
 
 module.exports = router;
