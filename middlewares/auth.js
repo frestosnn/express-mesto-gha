056cb0aa305c2a152333be73626857cca4203385
frestosnn <fresto.n@yandex.ru>
2023-11-07 16:45:48 +0300

    тест

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/unauthorized-errors');
+const jwt = require("jsonwebtoken");
+const UnauthorizedError = require("../errors/unauthorized-errors");
 
-const { JWT_SECRET = 'secret' } = process.env;
+const { JWT_SECRET = "secret" } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError('Необходима авторизация'));
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return next(new UnauthorizedError("Необходима авторизация"));
   }
 
-  const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace("Bearer ", "");
   let payload;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    return next(new UnauthorizedError("Необходима авторизация"));
   }
 
   req.user = payload;
